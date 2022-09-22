0a0d03437cc34e2082684caa71b9a2fd8a854d8f
Michal Swiderski <marketing@michal-swiderski.pl>
2022-09-22 21:36:34 +0200

    CU-2vznm6w - Aktualizacja strony do next js

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,7 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import styles from "../../styles/Header.module.css"
+
 import Image from "next/image";
 import Head from 'next/head'
 
-import styles from "../../styles/Header.module.css"
 
 
 const Header = () => {
@@ -11,6 +13,7 @@ const Header = () => {
         <title>Połączę dane w Twojej firmie</title>
         <meta name="description" content="Zajmuje się budowaniem konektorów danych dla firm potrzebujących scalać ze sobą dane." />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
     <div className={styles.wrapper}>
       <Image src="/images/logo_michal_swiderski.png" alt="Michał Świderski" width={150} height={150}/>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
-
 import Image from 'next/image'
 import Header from '../components/header/Header'
 import styles from '../styles/Home.module.css'
 
+
+
 export default function Home() {
   return (
     <>
+
+    <div class="container text-center">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
+
     <Header />
     <div className={styles.container}>
       <main className={styles.main}>
