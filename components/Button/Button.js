4e79d243502d81a989717eb65464d3eb175bad53
Michał Świderski <59454281+mskrotka@users.noreply.github.com>
2022-10-04 20:54:34 +0200

    CU-2vznm6w_Aktualizacja-strony-do-next-js (#5)
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2vznm6w - Aktualizacja strony do next js

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -2,15 +2,20 @@ import styles from "/styles/Button.module.css"
 
 import Link from 'next/link'
 
-const Button = ({ version, href, children }) => {
+const Button = ({ version, type, href, children }) => {
   const ver = version === "color" ? styles.color : styles.light
 
   return (
+    <>
+    {type === "Form" ?
+      <button type="submit" className={ver}>{children}</button>
+     :
     <div className={styles.wrapper}>
-        <Link href={href}>
-          <button className={ver}>{children}</button>
-        </Link>
-    </div>
+      <Link href={href}>
+        <button className={ver}>{children}</button>
+      </Link>
+    </div>}
+    </>
   )
 };
 
