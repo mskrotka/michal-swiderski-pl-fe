6c8991bbf1aaae5f6630585cc5a60ed4e55ac888
Michal Swiderski <marketing@michal-swiderski.pl>
2022-10-04 22:15:51 +0200

    CU-2vznm6w - Aktualizacja strony do next js

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import styles from "../../styles/Header.module.css"
 
+import Script from 'next/script'
 import Link from 'next/link'
 import Image from "next/image";
 import Head from 'next/head'
@@ -12,6 +13,19 @@ const Header = ({visible}) => {
 
   return (
     <>
+    <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', 'GTM-52K69DR');
+          `,
+        }}
+      />
       <Head>
         <title>Zmodernizuję Twoje procesy marketingowe i sprzedażowe</title>
         <meta name="description" content="Zajmuje się budowaniem procesów marketingowych i sprzedażowych. W ramach tego buduję konektory danych, ktre automatyzują obecne procesy." />
