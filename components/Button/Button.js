6f6cd8190839ee676e571d16c1a55ecd39754e52
Michał Świderski <59454281+mskrotka@users.noreply.github.com>
2022-10-05 17:16:53 +0200

    Development (#10)
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2vznm6w - Aktualizacja strony do next js (#4)
    
    * CU-2vznm6w_Aktualizacja-strony-do-next-js (#5)
    
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2vznm6w_Aktualizacja-strony-do-next-js (#6)
    
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2vznm6w_Aktualizacja-strony-do-next-js (#7)
    
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2xjy3rv_Poprawienie-mobile-i-dodanie-treci (#9)
    
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2vznm6w - Aktualizacja strony do next js
    
    * CU-2xjy3rv - Poprawienie mobile i dodanie treści
    
    * CU-2xjy3rv - Poprawienie mobile i dodanie treści

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -2,17 +2,20 @@ import styles from "/styles/Button.module.css"
 
 import Link from 'next/link'
 
-const Button = ({ version, type, href, children }) => {
+
+const Button = ({ version, type, href, children, ...props }) => {
   const ver = version === "color" ? styles.color : styles.light
 
   return (
     <>
     {type === "Form" ?
-      <button type="submit" className={ver}>{children}</button>
+      <button type="submit" className={ver} {...props}>{children}</button>
+     : type === "noLink" ?
+     <button className={`${ver} ${styles.small}`} {...props}>{children}</button>
      :
     <div className={styles.wrapper}>
       <Link href={href}>
-        <button className={ver}>{children}</button>
+        <button className={ver} {...props}>{children}</button>
       </Link>
     </div>}
     </>
